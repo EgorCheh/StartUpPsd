0fe213694b1abb2683df2c85e836cf20bc6548a6
EgorCheh <chehovich.egor@gmail.com>
2020-09-20 01:12:14 +0300

    Added of the works section

--- link.js
+++ link.js
@@ -1,5 +1,5 @@
 function funn(ev){
-    alert("asd")
+    alert("asd");
 }
 var slider__items = document.querySelectorAll('.slider__item');
 var back_limk;
@@ -11,16 +11,16 @@ for(let item of slider__items) {
        
        back_limk.style.background="rgba(0, 0, 0, .8)";
        back_limk.style.transitionDuration="0.5s";
+       
 
        team__links.style.visibility="visible";
        team__links.style.transitionDuration="0.3s";
        team__links.style.marginBottom="85px";
-    }
+    };
    item.onmouseout = function(event){
     team__links.style.marginBottom="55px";
     team__links.style.visibility="hidden";
     
     back_limk.style.background="none";
-    
-}
+};
 }
